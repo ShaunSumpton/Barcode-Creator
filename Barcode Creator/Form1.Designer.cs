de89cfa0b31b27d6d45b7d3a106b24199601b133
Shaun Sumpton <s.sumpton@agnortheast.com>
2019-07-09 15:32:12 +0100

    Creates output MRDF File

--- Barcode Creator/Form1.Designer.cs
+++ Barcode Creator/Form1.Designer.cs
@@ -34,10 +34,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -83,43 +83,43 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(70, 97);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Postcode Column";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(71, 71);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(73, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Name Column";
-            // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(12, 94);
+            this.textBox3.Location = new System.Drawing.Point(13, 68);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(52, 20);
             this.textBox3.TabIndex = 6;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(12, 68);
+            this.textBox4.Location = new System.Drawing.Point(12, 94);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(52, 20);
             this.textBox4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Postcode Index No.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Name Index No.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(268, 134);
+            this.ClientSize = new System.Drawing.Size(276, 146);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox3);
@@ -144,10 +144,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Barcode Creator/Form1.cs
+++ Barcode Creator/Form1.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Barcode_Creator
 {
     public partial class Form1 : Form
@@ -17,6 +18,7 @@ namespace Barcode_Creator
         public Form1()
         {
             InitializeComponent();
+            Application.DoEvents();
         }
 
 
@@ -25,11 +27,11 @@ namespace Barcode_Creator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int SER = 000001; // set serial number
-            string jn = textBox1.Text;
+            long SER = 000001; // set serial number
+            long jn = int.Parse(textBox1.Text);
             int numpg = int.Parse(textBox2.Text);
-            string name = textBox3.Text;
-            string pc = textBox4.Text;
+            int name = int.Parse(textBox3.Text) + numpg;
+            int pc = int.Parse(textBox4.Text) + numpg;
 
             OpenFileDialog openFileDialog2 = new OpenFileDialog();
             openFileDialog2.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
@@ -38,25 +40,99 @@ namespace Barcode_Creator
             file = openFileDialog2.FileName;
             dir = Path.GetDirectoryName(file);
 
+            string JobID = new string(' ', 10);
+            string PieceID = new string(' ', 6);
+            string JobType = new string(' ', 32);
+            string SourceID = new string(' ', 32);
+            var Name = new string(' ', 40);
+            string PagesInput02 = new string(' ', 2);
+            string PagesInput03 = new string(' ', 2);
+            string PagesInput04 = new string(' ', 2);
+            string SubsetInput01 = new string(' ', 2);
+            string SubsetInput02 = new string(' ', 2);
+            string SubsetInput03 = new string(' ', 2);
+            string SubsetInput04 = new string(' ', 2);
+            string StitchInput01 = new string(' ', 2);
+            string StitchInput02 = new string(' ', 2);
+            string StitchInput03 = new string(' ', 2);
+            string StitchInput04 = new string(' ', 2);
+            string InputWeight = new string(' ', 5);
+
+            int Sel1 = 0;
+            int Sel2 = 0;
+            int Sel3 = 0;
+            int Sel4 = 0;
+            int Sel5 = 0;
+            int Sel6 = 0;
+            int Sel7 = 0;
+            int Sel8 = 0;
+            int Sel9 = 0;
+            int Sel10 = 0;
+            int Sel11 = 0;
+            int Sel12 = 0;
+            int Sel13 = 0;
+            int Sel14 = 0;
+            int Sel15 = 0;
+            int Sel16 = 0;
+            int AccountPull = 0;
+            int QualityAudit = 0;
+            int AlertClear = 0;
+            int EdgeMark = 0;
+            int VS1 = 0;
+            int VS2 = 0;
+            int VS3 = 0;
+            int VS4 = 0;
+            int VS5 = 0;
+            int VS6 = 0;
+
+
+            string AccountIdentifier01 = new string(' ', 40);
+            string AccountIdentifier02 = new string(' ', 40);
+            string AccountIdentifier03 = new string(' ', 40);
+            string AccountIdentifier04 = new string(' ', 40);
+            string Address01 = new string(' ', 40);
+            string Address02 = new string(' ', 40);
+            string Address03 = new string(' ', 40);
+            string Address04 = new string(' ', 40);
+            string Address05 = new string(' ', 40);
+            string Address06 = new string(' ', 40);
+            var postcode = new string(' ', 16);
+            string PostcodeShort = new string(' ', 16);
+            string BRAddress01 = new string(' ', 40);
+            string BRAddress02 = new string(' ', 40);
+            string BRAddress03 = new string(' ', 40);
+            string BRAddress04 = new string(' ', 40);
+            string BRAddress05 = new string(' ', 40);
+            string ReprintIndex = new string(' ', 30);
+            string StartPageOffset = new string(' ', 6);
+            string PageCountOffset = new string(' ', 6);
+            string UserDefined01 = new string(' ', 40);
+            string UserDefined02 = new string(' ', 40);
+            string UserDefined03 = new string(' ', 40);
+            string UserDefined04 = new string(' ', 39);
+            string Filler01 = new string(' ', 2);
+
             using (var reader = new StreamReader(file)) //load CSV from BBS
             using (var csv = new CsvReader(reader))
 
-
+            
 
             {
-
+              
 
                 using (var dr = new CsvDataReader(csv))
                 {
                     var dt = new DataTable();
                     dt.Load(dr);
 
+                  
+
                     // Create new columns to be appended the start of the datatable
                     int i = 1;
                     int k = 0;
                     do
                     {
-                        DataColumn newCol = new DataColumn("pbBarcode" + i, typeof(string));
+                        DataColumn newCol = new DataColumn("PBbarcode" + i, typeof(string));
                         dt.Columns.Add(newCol);
                         newCol.SetOrdinal(k);
 
@@ -65,6 +141,9 @@ namespace Barcode_Creator
 
                     } while (i != numpg+1);
 
+                    DataColumn newCol1 = new DataColumn("SER", typeof(string));
+                    dt.Columns.Add(newCol1);
+                    newCol1.SetOrdinal(0);
 
 
                     // Add new columns for Barcodes
@@ -76,26 +155,66 @@ namespace Barcode_Creator
 
 
                     //loop through each row and add data
-        
+                    string str = new string(' ', 32);
+                    string OutDat;
+                  
+
+                    using (StreamWriter sw = new StreamWriter(dir + "\\"+ jn + " MRDF" + ".txt",append: true))
+                    {
+                        string header = jn.ToString("0000000000") + str + DateTime.Now;
+                        sw.WriteLine(header);
+                    }
+
+                    
+
+
                     i = 1;
+                    int y = 0;
+                   
+
+
                     foreach (DataRow row in dt.Rows)
                     {
+                        StringBuilder sb = new StringBuilder(40);
                         
-                            for (i = 1; i < numpg+1;i++)
+
+                        Name = dt.Rows[y][name].ToString();
+                        Name = Name.PadRight(40,' ');
+
+                        postcode = dt.Rows[y][pc].ToString();
+                        postcode = postcode.PadRight(16, ' ');
+
+                        y++;
+
+                        using (StreamWriter sw = new StreamWriter(dir + "\\" + jn + " MRDF" + ".txt", append: true))
+                        {
+                            OutDat = jn.ToString("0000000000") + SER.ToString("00000") + JobType + SourceID + numpg.ToString("00") + PagesInput02 + PagesInput03 + PagesInput04;
+                            OutDat = OutDat + SubsetInput01 + SubsetInput02 + SubsetInput03 + SubsetInput04 + StitchInput01 + StitchInput02 + StitchInput03 + StitchInput04 + InputWeight;
+                            OutDat = OutDat + Sel1 + Sel2 + Sel3 + Sel4 + Sel5 + Sel6 + Sel7 + Sel8 + Sel9 + Sel10 + Sel11 + Sel12 + Sel13 + Sel14 + Sel15 + Sel16;
+                            OutDat = OutDat + AccountPull + QualityAudit + AlertClear + EdgeMark + VS1 + VS2 + VS3 + VS4 + VS5 + VS6;
+                            OutDat = OutDat + AccountIdentifier01 + AccountIdentifier02 + AccountIdentifier03 + AccountIdentifier04;
+                            OutDat = OutDat + Name + Address01 + Address02 + Address03 + Address04 + Address05 + Address06 + postcode + PostcodeShort;
+                            OutDat = OutDat + BRAddress01 + BRAddress02 + BRAddress03 + BRAddress04 + BRAddress05;
+                            OutDat = OutDat + ReprintIndex + StartPageOffset + PageCountOffset;
+                            OutDat = OutDat + UserDefined01 + UserDefined02 + UserDefined03 + UserDefined04 + Filler01;
+                            sw.WriteLine(OutDat);
+
+
+                        }
+
+                        for (i = 1; i < numpg+1;i++)
                             {
-                                row["pbBarcode" + i] = SER.ToString("0000");
-                                
+
+                           
+                                row["SER"] = SER.ToString("000000");
+                                row["PBbarcode" + i] = jn.ToString("0000000000") + SER.ToString("000000") + i.ToString("00") + numpg.ToString("00") + ("00000000") ;
                             }
-
-
-
                         SER++;
-                     }
+                    }
 
+                        
 
-                        //}
-
-                        using (var textWriter = File.CreateText(dir + "\\" + "test1.txt"))
+                    using (var textWriter = File.CreateText(dir + "\\" + jn + ".txt")) // write contents to text file
                         using (var csv1 = new CsvWriter(textWriter))
                         {
                             // Write columns
@@ -121,6 +240,9 @@ namespace Barcode_Creator
                     }
                 }
             }
-        }
+
+        
+
+    }
     }
 
